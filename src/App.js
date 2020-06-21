03828acc99d48bc5db7ca30df39eb2c3d4025e0f
Vaibhav <varora@catalystone.com>
2020-06-21 15:24:46 +0530

    refactor: 💡 add playerName to controller props, and refactor

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React, { useState } from 'react';
 import { Controller } from './components/controller/controller';
 
+const pageCenterStyle = {
+  display: 'flex',
+  justifyContent: 'center',
+  height: '100vh',
+  alignItems: 'center'
+}
+
 function App() {
   let [input, setInput] = useState('');
+  let [playerName, setName] = useState(input);
+
+  const updatePlayerName = () => {
+    setName(input);
+    setInput('');
+  };
+  const isInputEmpty = () => input.trim().length === 0;
+
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', height: '100vh', alignItems: 'center' }}>
+    <div style={pageCenterStyle}>
       <div style={{ display: 'inline-flex', flexDirection: 'column' }}>
         <h1> Snake Game </h1>
-        <input
+        <input value={input}
           style={{ marginBottom: '0.5rem' }}
           placeholder="Your name"
           onChange={(event) => setInput(event.target.value)} />
-        <button disabled={input.trim().length === 0}> Play </button>
-        <Controller></Controller>
+        <button disabled={isInputEmpty()} onClick={updatePlayerName}> Play </button>
+        <Controller player={{ name: playerName }}></Controller>
       </div>
     </div>
   );
