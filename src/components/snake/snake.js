d37e6fb0f1a143a250c456df4acff1ca6fb17fa0
Vaibhav <varora@catalystone.com>
2020-06-28 13:33:22 +0530

    WIP: add snake component

---
+++ src/components/snake/snake.js
@@ -0,0 +1,12 @@
+import { useEffect } from "react";
+
+const Snake = (props) => {
+    useEffect(() => {
+        if (props.canvas.current) {
+            const canvas = props.canvas.current;
+        }
+    });
+    return '';
+}
+
+export { Snake }
